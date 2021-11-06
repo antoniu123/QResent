b480f2a399f1b611d231c24aacc05382ef23b96a
Bogdan ANTONIU (101715) <bogdan.antoniu@stud.acs.upb.ro>
2021-11-06 19:22:57 +0200

    database now is in same directory with application

--- src/main/java/com/example/qresent/repository/StudentMateriePrezentaRepository.java
+++ src/main/java/com/example/qresent/repository/StudentMateriePrezentaRepository.java
@@ -2,6 +2,15 @@ package com.example.qresent.repository;
 
 import com.example.qresent.model.Student_Materie_Prezenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
 
 public interface StudentMateriePrezentaRepository extends JpaRepository <Student_Materie_Prezenta, Long> {
+	@Modifying(clearAutomatically = true)
+	@Query("update Student_Materie_Prezenta e set e.prezenta=1 where e.uuid = :uuid")
+	int setPrezenta(@Param("uuid")UUID uuid);
+
 }
